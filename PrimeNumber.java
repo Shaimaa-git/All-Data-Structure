3e3d72fd3fff778e137af544a61c0acd16b6ba1d
shaimaa-git <shaimaasaidmo@gmail.com>
2024-03-11 08:20:39 +0400

    check if the entered number is prime or not

---
+++ PrimeNumber.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PrimeNumberChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        scanner.close();
+    }
+
+    // Function to check if a number is prime
+    private static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        // Check for divisibility from 2 to the square root of the number
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false; // Not a prime number
+            }
+        }
+
+        return true; // Prime number
+    }
+}
